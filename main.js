ce09cda3be44d4488a32e0444345109335fa110c
BCPSmiles <118627675+vcdvfghj@users.noreply.github.com>
2025-04-25 07:03:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function StudentPortal() {
+    var password = document.getElementById('password').value;
+    if (password == "password") {
+        window.location.href = "student.html";
+    }
+}
